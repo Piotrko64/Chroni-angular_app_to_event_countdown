3384f48ea3cfc27792227e59e7113e6c284cb925
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-11-07 18:15:48 +0100

    Start creating components for SVG images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnimatedWallpaperComponent } from './components/auth-page/animated-wallpaper/animated-wallpaper.component';
+import { AuthPageComponent } from './pages/auth-page/auth-page.component';
+import { MarkFiveComponent } from './components/auth-page/animated-wallpaper/SVG/mark-five/mark-five.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnimatedWallpaperComponent
+    AnimatedWallpaperComponent,
+    AuthPageComponent,
+    MarkFiveComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/auth-page/animated-wallpaper/SVG/mark-five/mark-five.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mark-five',
+  templateUrl: './mark-five.component.html',
+  styleUrls: ['./mark-five.component.scss'],
+})
+export class MarkFiveComponent {}
